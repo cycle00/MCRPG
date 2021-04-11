c87e4aa7977a6378e097109ff203747ab8128aca
BuildTools <unconfigured@null.spigotmc.org>
2021-04-10 22:38:33 -0400

    b ruh moment

---
+++ src/main/java/cycle00/mcrpg/Main.java
@@ -0,0 +1,12 @@
+package cycle00.mcrpg;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+@SuppressWarnings("unused")
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        
+    }
+}
